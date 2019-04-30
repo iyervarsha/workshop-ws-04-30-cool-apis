d1167e766d583ca5de3e6a51b070f88a2470e944
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-30 15:24:57 -0400

    project

--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ app.post('/', upload.single('file'), (req, res, next) => {
     body: fs.createReadStream(req.file.path),
     auth: {
       user: 'apikey',
-      pass: '<YOUR API KEY>',
+      pass: '<Qq63cSR33QJMzCtphdWpvaEuGAxKzSfMCaE5rtGsWkbD>',
     },
   }, (error, response, body) => {
     if (error) {
